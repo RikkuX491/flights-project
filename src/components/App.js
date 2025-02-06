71405dbacd69e178e2134d844685e53f34d309bf
Ricardo Guerra <RikkuG491@gmail.com>
2025-02-06 14:45:18 -0500

    Added children routes logic

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
+import { Outlet } from "react-router-dom";
+
 import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
       <Header/>
+      <Outlet/>
     </div>
   );
 }
