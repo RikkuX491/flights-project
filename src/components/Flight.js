7b0aea1b69615848e277a2f5dcf1f36589e578ac
Ricardo Guerra <RikkuG491@gmail.com>
2025-02-06 15:32:24 -0500

    Added more data in <Flight> component

--- src/components/Flight.js
+++ src/components/Flight.js
@@ -1,8 +1,13 @@
 function Flight({flight}){
-    console.log(flight)
-
     return (
-        <img src={flight.image}/>
+        <li>
+            <h2>Flight # {flight.id}</h2>
+            <img src={flight.image} alt={flight.airline}/>
+            <h2>Airline: {flight.airline}</h2>
+            <h2>Price: ${flight.price}</h2>
+            <h2>Origin: {flight.origin}</h2>
+            <h2>Destination: {flight.destination}</h2>
+        </li>
     )
 }
 
