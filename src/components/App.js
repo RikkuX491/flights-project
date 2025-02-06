a3a42debbf24d65443c31d6e16090330c7725cf9
Ricardo Guerra <RikkuG491@gmail.com>
2025-02-06 15:18:07 -0500

    Added logic to retrieve flights data from the server

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,23 @@
 import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 import Header from "./Header";
 import NavBar from "./NavBar";
 
 function App() {
+
+  const [flights, setFlights] = useState([])
+
+  console.log(flights)
+
+  useEffect(retrieveFlights, [])
+
+  function retrieveFlights(){
+    fetch('http://localhost:4000/flights')
+    .then(response => response.json())
+    .then(setFlights)
+  }
+
   return (
     <div className="app">
       <NavBar/>
